99dd2cf21cea75feba9fa587dcaac6e86c244a47
tchiwewe <tapiwa.chiwewe@up.ac.za>
2015-09-24 19:38:15 +0200

    Changes to monitor performance

---
+++ lib/debug_utils.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDED_COGNITIVA_DEBUG_UTILS_H
+#define INCLUDED_COGNITIVA_DEBUG_UTILS_H
+
+#define DEBUG_VERBOSE 1
+#define DEBUG_INFO 2
+#define DEBUG_PERFORMANCE 4
+#define DEBUG_ALL 255
+
+namespace gr {
+namespace cognitiva {
+} // namespace cognitiva
+} // namespace gr
+
+#endif /* INCLUDED_COGNITIVA_DEBUG_UTILS_H */
